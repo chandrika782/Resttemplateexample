97ce453fd22cbf0d0a1af51c61d869be00a2456a
User1 <User1@LP-CNU208134K.HCLT.CORP.HCL.IN>
2019-07-31 18:00:43 +0530

    rest template

---
+++ src/main/java/com/products/Products/model/Employee.java
@@ -0,0 +1,53 @@
+package com.products.Products.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+private int empId;
+private String empName;
+private String address;
+private long phoneno;
+private String gender;
+public int getEmpId() {
+	return empId;
+}
+public void setEmpId(int empId) {
+	this.empId = empId;
+}
+public String getEmpName() {
+	return empName;
+}
+public void setEmpName(String empName) {
+	this.empName = empName;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public long getPhoneno() {
+	return phoneno;
+}
+public void setPhoneno(long phoneno) {
+	this.phoneno = phoneno;
+}
+public String getGender() {
+	return gender;
+}
+public void setGender(String gender) {
+	this.gender = gender;
+}
+
+
+}
